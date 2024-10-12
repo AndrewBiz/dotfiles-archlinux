48befde6be1ffbd99a65894e500f67808a07d55a
Andrew Biz <andrew.bizyaev@gmail.com>
2024-10-12 20:02:11 +0300

    bashrc fish + rust

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,6 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    
+
     # atuin
     atuin init fish | source
+
+    # rust
+    source "$HOME/.cargo/env.fish"
 end
